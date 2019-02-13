05a83ef91e97c51f1e5ac9003db1e3b3078a69d8
kritesh1234 <47594219+kritesh1234@users.noreply.github.com>
2019-02-13 19:32:53 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js") 
+const settings = require("./your_settings.json")
+const bot = new Discord.Client()
+bot.on('ready', async => {
+console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
+});
